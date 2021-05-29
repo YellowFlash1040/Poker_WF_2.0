2f03496b3d735df46cc62c476257e886dbd71bdd
Andrew <andrejkovtunec@gmail.com>
2021-05-29 16:20:00 +0300

    Изменены начальные банки игроков

--- Poker_WF/Form1.cs
+++ Poker_WF/Form1.cs
@@ -69,12 +69,12 @@ namespace Poker_WF
             TableControls.Add(GameBank_Value_Label);
             TableControls.Add(GameBank_label);
 
-            GraphicPokerPlayer FirstPlayer = new GraphicPokerPlayer("FirstPlayer", new GraphicCardSet(FirstPlayer_panel), 1000,
+            GraphicPokerPlayer FirstPlayer = new GraphicPokerPlayer("FirstPlayer", new GraphicCardSet(FirstPlayer_panel), 500,
                FirstPlayer_BankValue_label, FirstPlayer_BetValue_label, FirstPlayer_Raise_label, FirstPlayer_Check_label,
                FirstPlayer_Fold_label, FirstPlayer_Call_label, FirstPlayer_ConfirmRaiseBet_button,
                FirstPlayer_CanselRaiseBet_button, FirstPlayer_EnterValueOfRaiseBet_textBox, FirstPlayer_Winner_label,
                FirstPlayerCombination_Label);
-            GraphicPokerPlayer SecondPlayer = new GraphicPokerPlayer("SecondPlayer", new GraphicCardSet(SecondPlayer_panel), 1000,
+            GraphicPokerPlayer SecondPlayer = new GraphicPokerPlayer("SecondPlayer", new GraphicCardSet(SecondPlayer_panel), 2000,
                 SecondPlayer_BankValue_label, SecondPlayer_BetValue_label, SecondPlayer_Raise_label, SecondPlayer_Check_label,
                 SecondPlayer_Fold_label, SecondPlayer_Call_label, SecondPlayer_ConfirmRaiseBet_button,
                 SecondPlayer_CanselRaiseBet_button, SecondPlayer_EnterValueOfRaiseBet_textBox, SecondPlayer_Winner_label,
@@ -84,7 +84,7 @@ namespace Poker_WF
                 ThirdPlayer_Fold_label, ThirdPlayer_Call_label, ThirdPlayer_ConfirmRaiseBet_button,
                 ThirdPlayer_CanselRaiseBet_button, ThirdPlayer_EnterValueOfRaiseBet_textBox, ThirdPlayer_Winner_label,
                 ThirdPlayerCombination_Label);
-            GraphicPokerPlayer FourthPlayer = new GraphicPokerPlayer("FourthPlayer", new GraphicCardSet(FourthPlayer_panel), 1000,
+            GraphicPokerPlayer FourthPlayer = new GraphicPokerPlayer("FourthPlayer", new GraphicCardSet(FourthPlayer_panel), 900,
                 FourthPlayer_BankValue_label, FourthPlayer_BetValue_label, FourthPlayer_Raise_label, FourthPlayer_Check_label,
                 FourthPlayer_Fold_label, FourthPlayer_Call_label, FourthPlayer_ConfirmRaiseBet_button,
                 FourthPlayer_CanselRaiseBet_button, FourthPlayer_EnterValueOfRaiseBet_textBox, FourthPlayer_Winner_label,
