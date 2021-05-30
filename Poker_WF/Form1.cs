33ae34a666638bd00d8aa0cb413df16a5e589989
varjagstas <ctanislavkrohmal1@gmail.com>
2021-05-30 10:04:20 +0300

    30.05

--- Poker_WF/Form1.cs
+++ Poker_WF/Form1.cs
@@ -69,22 +69,33 @@ namespace Poker_WF
             TableControls.Add(GameBank_Value_Label);
             TableControls.Add(GameBank_label);
 
-            GraphicPokerPlayer FirstPlayer = new GraphicPokerPlayer("FirstPlayer", new GraphicCardSet(FirstPlayer_panel), 500,
+            Random random = new Random();
+            int[] playersBanks = new int[4];
+            for (int i = 0; i < playersBanks.Length; i++)
+            {
+                playersBanks[i] = (int)((random.Next(9) / 10.0 + random.Next(1, 3)) * 1000);
+            }
+
+            GraphicPokerPlayer FirstPlayer = new GraphicPokerPlayer("Alexander", new GraphicCardSet(FirstPlayer_panel), 1000,
+                FirstPlayer_Name_label,
                FirstPlayer_BankValue_label, FirstPlayer_BetValue_label, FirstPlayer_Raise_label, FirstPlayer_Check_label,
                FirstPlayer_Fold_label, FirstPlayer_Call_label, FirstPlayer_ConfirmRaiseBet_button,
                FirstPlayer_CanselRaiseBet_button, FirstPlayer_EnterValueOfRaiseBet_textBox, FirstPlayer_Winner_label,
                FirstPlayerCombination_Label);
-            GraphicPokerPlayer SecondPlayer = new GraphicPokerPlayer("SecondPlayer", new GraphicCardSet(SecondPlayer_panel), 2000,
+            GraphicPokerPlayer SecondPlayer = new GraphicPokerPlayer("Mihail", new GraphicCardSet(SecondPlayer_panel), 1200,
+                SecondPlayer_Name_label,
                 SecondPlayer_BankValue_label, SecondPlayer_BetValue_label, SecondPlayer_Raise_label, SecondPlayer_Check_label,
                 SecondPlayer_Fold_label, SecondPlayer_Call_label, SecondPlayer_ConfirmRaiseBet_button,
                 SecondPlayer_CanselRaiseBet_button, SecondPlayer_EnterValueOfRaiseBet_textBox, SecondPlayer_Winner_label,
                 SecondPlayerCombination_Label);
-            GraphicPokerPlayer ThirdPlayer = new GraphicPokerPlayer("ThirdPlayer", new GraphicCardSet(ThirdPlayer_panel), 1000,
+            GraphicPokerPlayer ThirdPlayer = new GraphicPokerPlayer("Oleg", new GraphicCardSet(ThirdPlayer_panel), 1300,
+                ThirdPlayer_Name_label,
                 ThirdPlayer_BankValue_label, ThirdPlayer_BetValue_label, ThirdPlayer_Raise_label, ThirdPlayer_Check_label,
                 ThirdPlayer_Fold_label, ThirdPlayer_Call_label, ThirdPlayer_ConfirmRaiseBet_button,
                 ThirdPlayer_CanselRaiseBet_button, ThirdPlayer_EnterValueOfRaiseBet_textBox, ThirdPlayer_Winner_label,
                 ThirdPlayerCombination_Label);
-            GraphicPokerPlayer FourthPlayer = new GraphicPokerPlayer("FourthPlayer", new GraphicCardSet(FourthPlayer_panel), 900,
+            GraphicPokerPlayer FourthPlayer = new GraphicPokerPlayer("Nikolai", new GraphicCardSet(FourthPlayer_panel), 2000,
+                fourthPlayer_Name_label,
                 FourthPlayer_BankValue_label, FourthPlayer_BetValue_label, FourthPlayer_Raise_label, FourthPlayer_Check_label,
                 FourthPlayer_Fold_label, FourthPlayer_Call_label, FourthPlayer_ConfirmRaiseBet_button,
                 FourthPlayer_CanselRaiseBet_button, FourthPlayer_EnterValueOfRaiseBet_textBox, FourthPlayer_Winner_label,
